6da78ba4701bbe1258f679cc2bf0536857bae82d
ayaanmondal <ayanmondal9929@gmail.com>
2021-07-15 20:42:44 +0530

    Initial Application

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <div className="App">
+      Click to Eat
+    </div>
+  );
+}
+
+export default App;
