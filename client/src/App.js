053d0eacfa11da4d6aa36ed74f6cce2fc00f61e8
ayaanmondal <ayanmondal9929@gmail.com>
2021-07-20 03:00:53 +0530

    Cart Screen

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,19 @@ import './App.css';
 import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar';
 import Homescreen from './screens/Homescreen';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import Cartscreen from './screens/Cartscreen';
+
+
+
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Homescreen/>
+      <BrowserRouter>
+        <Route path="/" exact component={Homescreen}></Route>
+        <Route path="/cart" exact component={Cartscreen}></Route>
+      </BrowserRouter>
     </div>
   );
 }
